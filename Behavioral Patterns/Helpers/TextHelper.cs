38501ffc999e7217423cfdc50ce53110c4f915ad
Peter Velkov <kidroca@gmail.com>
2015-09-28 21:02:52 +0300

    Updated the homework helper

--- Behavioral Patterns/Helpers/TextHelper.cs
+++ Behavioral Patterns/Helpers/TextHelper.cs
@@ -4,26 +4,37 @@
 
     public class TextHelper
     {
-        public void SetupConsole()
+        public void SetupConsole(
+            ConsoleColor color = ConsoleColor.White
+            , ConsoleColor background = ConsoleColor.Gray
+            , int windowWidth = 120)
         {
-            Console.Clear();
             try
             {
-                Console.SetWindowSize(120, Console.WindowHeight);
+                Console.BackgroundColor = background;
+                Console.ForegroundColor = color;
+                Console.OutputEncoding = System.Text.Encoding.Unicode;
+                Console.SetWindowSize(windowWidth, Console.WindowHeight);
             }
-            catch (ArgumentOutOfRangeException ex)
+            catch (Exception)
             {
-                // Prevent Crashing if Console window is set too wide
+                // Prevent Crashing if Console can't take any of the parameters
+            }
+            finally
+            {
+                Console.Clear();
             }
         }
 
         public void Restart(Action restartCallback)
         {
-            this.PrintColorText("\n\nPRESS ANY KEY TO RESTART \n(Ctrl + C to exit)\n\n", "red");
+            Console.WriteLine();
+            this.PrintHeading("PRESS ANY KEY TO RESTART or Ctrl + C to exit");
             Console.CursorVisible = false;
             Console.ReadKey(true);
             Console.CursorVisible = true;
-            restartCallback(); 
+
+            restartCallback();
         }
 
         public void PrintColorText(string text, ConsoleColor color)
@@ -69,6 +80,36 @@
                     Console.Write(text);
                     break;
             }
+        }
+
+        public string ReadConsoleInColor(ConsoleColor color)
+        {
+            var previousColor = Console.ForegroundColor;
+
+            Console.ForegroundColor = color;
+            string input = Console.ReadLine();
+
+            Console.ForegroundColor = previousColor;
+
+            return input;
+        }
+
+        public void PrintHeading(string text, ConsoleColor color = ConsoleColor.White)
+        {
+            int totalWidth = Console.WindowWidth;
+            string format = string.Format(" {0} ", text);
+            char paddingChar = ' ';
+            var paddingColor = ConsoleColor.White;
+            var backgorundColor = ConsoleColor.DarkGray;
+            int freeWidth = totalWidth - format.Length;
+
+            this.PrintColorText(new string(paddingChar, totalWidth), paddingColor, backgorundColor);
+            this.PrintColorText(new string(paddingChar, freeWidth / 2), paddingColor, backgorundColor);
+            this.PrintColorText(format, color, backgorundColor);
+            this.PrintColorText(new string(paddingChar, freeWidth / 2), paddingColor, backgorundColor);
+            this.PrintColorText(new string(paddingChar, totalWidth), paddingColor, backgorundColor);
+            Console.WriteLine();
+            Console.WriteLine();
         }
     }
 }
